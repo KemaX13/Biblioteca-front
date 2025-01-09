dd99ecc150f71b876bae14802a8bfd022ed26a2d
Genta Mateo Kamekawa Roa <gentaroa1@gmail.com>
2025-01-09 14:29:42 +0100

    Readaptar al nuevo diseño

--- src/layout/User/LayoutUser.jsx
+++ src/layout/User/LayoutUser.jsx
@@ -7,29 +7,25 @@ function LayoutUser() {
     <div className="LayoutUser">
       <div className="headerUser">
         <div className="logo">
-          <img src="/src/assets/logo.png" alt="vocespropias" className="logo" />
-          <p class="tagline">literatura desde los márgenes</p>
-
+          <img src="/src/assets/logo.png" alt="biblioteca-llucmajor" className="logoImg"/>
         </div>
         <div className="menu-mobile">
           <img src="/src/assets/mobile-menu.png" alt="Menú" className="icon-mobile" />
           <img src="/src/assets/user.png" alt="Usuario" className="icon-mobile" />
           <img src="/src/assets/fav.png" alt="Favoritos" className="icon-mobile" />
-          <img src="/src/assets/cart.png" alt="Carrito" className="icon-mobile" />
+          <img src="/src/assets/search.png" alt="Buscador" className="icon-mobile" />
         </div>
         <div className="menu-desktop">
           <nav className="menu-desktop">
             <ul className="menu-categories">
-              <li><a href="#narrativa">Narrativa</a></li>
-              <li><a href="#poesia">Poesía</a></li>
-              <li><a href="#ensayo">Ensayo</a></li>
-              <li><a href="#infantil">Infantil</a></li>
-              <li><a href="#sobre-nosotras">Sobre nosotras</a></li>
+              <li><a href="#categorias">Categorías</a></li>
+              <li><a href="#actividades">Actividades</a></li>
+              <li><a href="#tu-biblioteca">Tu biblioteca</a></li>
             </ul>
             <div className="icons-desktop">
               <img src="/src/assets/user.png" alt="Usuario" className="icon-desktop" />
               <img src="/src/assets/fav.png" alt="Favoritos" className="icon-desktop" />
-              <img src="/src/assets/cart.png" alt="Carrito" className="icon-desktop" />
+              <img src="/src/assets/search.png" alt="Buscador" className="icon-desktop" />
             </div>
           </nav>
         </div>
