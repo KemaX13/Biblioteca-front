01c482db5df452e3d9f086383289b8777b4c308f
Genta Mateo Kamekawa Roa <gentaroa1@gmail.com>
2025-01-13 10:53:52 +0100

    temp hotfix

--- src/layout/User/LayoutUser.jsx
+++ src/layout/User/LayoutUser.jsx
@@ -4,22 +4,19 @@ import "./LayoutUser.css";
 
 function LayoutUser() {
   return (
-    <section className="LayoutUser">
-      <section className="headerUser">
-        <header className="logo">
+    <div className="LayoutUser">
+      <div className="headerUser">
+        <div className="logo">
           <img src="/src/assets/logo.png" alt="biblioteca-llucmajor" className="logoImg"/>
-          <img className="bg-img" src="/src/assets/bg-biblioteca.jpg" alt="bg-biblioteca-llucmajor" className="logoImg"/>
-        </header>
-
+        </div>
         <div className="menu-mobile">
           <img src="/src/assets/mobile-menu.png" alt="Menú" className="icon-mobile" />
           <img src="/src/assets/user.png" alt="Usuario" className="icon-mobile" />
           <img src="/src/assets/fav.png" alt="Favoritos" className="icon-mobile" />
           <img src="/src/assets/search.png" alt="Buscador" className="icon-mobile" />
         </div>
-
         <div className="menu-desktop">
-          <nav className="menu-desktop-nav">
+          <nav className="menu-desktop">
             <ul className="menu-categories">
               <li><a href="#categorias">Categorías</a></li>
               <li><a href="#actividades">Actividades</a></li>
@@ -32,10 +29,9 @@ function LayoutUser() {
             </div>
           </nav>
         </div>
-      </section>
-      
+      </div>
       <Outlet />
-    </section>
+    </div>
   );
 }
 
